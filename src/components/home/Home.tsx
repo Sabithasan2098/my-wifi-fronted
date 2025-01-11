8446b7aae41325d21cea3de7b3ab9ce1a7a10ecf
Sabithasan2098 <sabithasan2098@gmail.com>
2025-01-11 09:14:31 +0600

    add payment.section

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -20,63 +20,112 @@ const Home = () => {
   console.log(data.data.map((a: any) => a.name));
 
   return (
-    <div className="max-w-7xl mx-auto mt-5">
-      <h1 className="text-2xl md:text-5xl font-bold text-center mb-2">
-        Broadband users
-      </h1>
-      <table className="table text-center">
-        {/* head */}
-        <thead>
-          <tr className="bg-orange-500  text-white text-2xl font-normal ">
-            <th>#</th>
-            <th className="text-start">Name</th>
-            <th className="text-start">Mac Address</th>
-            <th>Date</th>
-            <th>Hold up</th>
-            <th>Status</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        {data?.data?.map((item: any, index: number) => (
-          <tbody key={index} className="mt-6">
-            <tr className="border border-orange-200 ">
-              <td>
-                <p>{index + 1}</p>
-              </td>
-              {item.status === "Owner" ? (
-                <td>
-                  <h1 className="text-sm text-start text-red-500">
-                    {item.name}
-                  </h1>
-                </td>
-              ) : (
-                <td>
-                  <h1 className="text-sm opacity-50 text-start">{item.name}</h1>
-                </td>
-              )}
-              <td>
-                <h1 className="text-start">{item.macAddress}</h1>
-              </td>
-              <td>
-                <h1>{item.date}</h1>
-              </td>
-              <td>
-                <p>{item.holdUp}</p>
-              </td>
-              <td>
-                <p>{item.payment}</p>
-              </td>
-              {item.status === "Owner" ? (
-                <th>no action</th>
-              ) : (
-                <th>
-                  <button className="btn btn-ghost btn-xs">action</button>
-                </th>
-              )}
+    <div>
+      <div className="hidden md:block max-w-7xl mx-auto mt-5">
+        <h1 className="text-2xl md:text-5xl font-bold text-center mb-2">
+          Broadband users
+        </h1>
+        <table className="table text-center">
+          {/* head */}
+          <thead>
+            <tr className="bg-orange-500  text-white text-2xl font-normal ">
+              <th>#</th>
+              <th className="text-start">Name</th>
+              <th className="text-start">Mac Address</th>
+              <th>Date</th>
+              <th>Hold up</th>
+              <th>Status</th>
+              <th>Action</th>
             </tr>
-          </tbody>
-        ))}
-      </table>
+          </thead>
+          {data?.data?.map((item: any, index: number) => (
+            <tbody key={index} className="mt-6">
+              <tr className="border border-orange-200 ">
+                <td>
+                  <p>{index + 1}</p>
+                </td>
+                {item.status === "Owner" ? (
+                  <td>
+                    <h1 className="text-sm text-start text-red-500">
+                      {item.name}
+                    </h1>
+                  </td>
+                ) : (
+                  <td>
+                    <h1 className="text-sm opacity-50 text-start">
+                      {item.name}
+                    </h1>
+                  </td>
+                )}
+                <td>
+                  <h1 className="text-start">{item.macAddress}</h1>
+                </td>
+                <td>
+                  <h1>{item.date}</h1>
+                </td>
+                <td>
+                  <p>{item.holdUp}</p>
+                </td>
+                <td>
+                  <p>{item.payment}</p>
+                </td>
+                {item.status === "Owner" ? (
+                  <th>no action</th>
+                ) : (
+                  <th>
+                    <button className="btn btn-ghost btn-xs">action</button>
+                  </th>
+                )}
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+      <div className="block lg:hidden md:hidden max-w-7xl mx-auto mt-5">
+        <h1 className="text-2xl md:text-5xl font-bold text-center mb-2">
+          Broadband users
+        </h1>
+        <table className="table text-center">
+          {/* head */}
+          <thead>
+            <tr className="bg-orange-500  text-white  ">
+              <th>#</th>
+              <th className="text-start">Name</th>
+              <th className="text-start">Mac Address</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          {data?.data?.map((item: any, index: number) => (
+            <tbody key={index} className="mt-6">
+              <tr className="border border-orange-200 ">
+                <td>
+                  <p>{index + 1}</p>
+                </td>
+                {item.status === "Owner" ? (
+                  <td>
+                    <h1 className="text-sm text-start text-red-500">
+                      {item.name}
+                    </h1>
+                  </td>
+                ) : (
+                  <td>
+                    <h1 className="text-sm opacity-50 text-start">
+                      {item.name}
+                    </h1>
+                  </td>
+                )}
+                <td>
+                  <h1 className="text-start">{item.macAddress}</h1>
+                </td>
+
+                <td>
+                  <p>{item.payment}</p>
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
     </div>
   );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,60 +1,39 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const nav = (
+    <>
+      <li>
+        <Link href={"/"}>Home</Link>
+      </li>
+      <li>
+        <Link href={"/Payment"}>Payment</Link>
+      </li>
+    </>
+  );
   return (
     <div className="navbar bg-base-100 max-w-7xl mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <h2>My Wifi</h2>
+            <Link href={"/"}>
+              <h2>My Wifi</h2>
+            </Link>
           </div>
           <ul
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {nav}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">My Wifi</a>
+        <Link href={"/"}>
+          <p className="btn btn-ghost text-xl">My Wifi</p>
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{nav}</ul>
       </div>
       <div className="navbar-end">
         <a className="btn">Button</a>
